fdf2b318aad8250970acec715a6fd4c6b226cb37
Vladlena Lynko <92115834+Vladoochka@users.noreply.github.com>
2022-04-11 00:28:00 +0300

    Update schemas.py

--- schemas.py
+++ schemas.py
@@ -112,29 +112,3 @@ GET_TODOS = {
     },
     'required': ['userId', 'id', 'title', 'completed']
 }
-
-
-class Resp:
-    def __init__(self, response):
-        self.response = response
-        self.response_status = response.status_code
-        self.response_headers = response.headers['content-type']
-
-    def assert_status_code(self, status_code):
-        if isinstance(status_code, list):
-            assert self.response_status in status_code
-        else:
-            assert self.response_status == status_code
-        return self
-
-    def assert_headers(self, headers):
-        if isinstance(headers, list):
-            assert self.response_headers in headers
-        else:
-            assert self.response_headers == headers
-        return self
-
-    def assert_support(self):
-        response_body = self.response.json()["support"]
-        assert response_body['url'] == "https://reqres.in/#support-heading"
-        assert response_body['text'] == "To keep ReqRes free, contributions towards server costs are appreciated!"
